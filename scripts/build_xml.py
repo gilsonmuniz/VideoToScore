4149b4a521c26998fea97311cc01927bc1d9fd91
GilsonMuniz <gilsonj725@gmail.com>
2025-04-05 11:44:23 -0300

    notes names comments on test music dict

--- scripts/build_xml.py
+++ scripts/build_xml.py
@@ -114,31 +114,31 @@ MARRIED_LIFE = {
 TEST = {
     'C3': {
         'instants': [29],
-        'durations': [1]
+        'durations': [1] # semifusa = 1
     },
     'D3': {
         'instants': [88],
-        'durations': [2]
+        'durations': [2] # fusa = 2
     },
     'E3': {
         'instants': [148],
-        'durations': [3]
+        'durations': [3] # semicolcheia = 4
     },
     'F3': {
         'instants': [207],
-        'durations': [6]
+        'durations': [6] # colcheia = 8
     },
     'G3': {
         'instants': [264],
-        'durations': [14]
+        'durations': [14] # semínima = 16
     },
     'A3': {
         'instants': [321],
-        'durations': [30]
+        'durations': [30] # mínima = 32
     },
     'B3': {
         'instants': [379],
-        'durations': [58]
+        'durations': [58] # semibreve = 64
     }
 }
 
