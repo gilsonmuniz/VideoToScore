5aa4073a79f1d2114e232629d325c1cd65621efd
GilsonMuniz <gilsonj725@gmail.com>
2025-04-10 22:20:59 -0300

    updating format_music_values

--- scripts/format_music_values.py
+++ scripts/format_music_values.py
@@ -19,9 +19,10 @@ def get_first_note_instant(music):
 def format_note_duration(note_duration):
     i = 6
     while note_duration < STANDARD_NOTES_VALUES[i]: i -= 1
-    note_duration_standard = (note_duration // STANDARD_NOTES_VALUES[i]) * STANDARD_NOTES_VALUES[i]
-    note_duration_dotted = note_duration_standard + STANDARD_NOTES_VALUES[i - 1]
-    return min(note_duration_standard, note_duration_dotted, key=lambda n: abs(n - note_duration))
+    duration_standard = (note_duration // STANDARD_NOTES_VALUES[i]) * STANDARD_NOTES_VALUES[i]
+    duration_dotted = duration_standard + (STANDARD_NOTES_VALUES[i - 1] if i else 0)
+    closest_formatted_duration = min(duration_standard, duration_dotted, key=lambda n: abs(n - note_duration))
+    return closest_formatted_duration
 
 def format_note_instant(note_instant, first_note_instant):
     return note_instant - first_note_instant
