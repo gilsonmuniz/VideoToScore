bb349131cdd0e682cfd30db929e6893f4873e35a
GilsonMuniz <gilsonj725@gmail.com>
2025-04-27 10:52:28 -0300

    build_irreducible need to remove ligatures

--- scripts/build_irreducible_musical_score.py
+++ scripts/build_irreducible_musical_score.py
@@ -1,7 +1,10 @@
 import xml.etree.ElementTree as ET
 from itertools import groupby
 
-DIVISIONS = 1  # 1 semifusa = 1 divisão
+DIVISIONS = 16  # Agora base é 16 para que a semifusa tenha duration=1
+
+STANDARD_NOTES_VALUES = (1, 2, 4, 8, 16, 32, 64)
+
 
 def parse_note_name(name):
     if len(name) == 2:
@@ -14,8 +17,13 @@ def parse_note_name(name):
         octave = int(name[-1])
     return step, alter, octave
 
-def create_note_element(step, alter, octave, tie_start=False, tie_stop=False):
+
+def create_single_note(step, alter, octave, voice, is_chord=False, tie_start=False, tie_stop=False, beam_type=None):
     note = ET.Element('note')
+
+    if is_chord:
+        ET.SubElement(note, 'chord')
+
     pitch = ET.SubElement(note, 'pitch')
     ET.SubElement(pitch, 'step').text = step
     if alter != 0:
@@ -23,29 +31,39 @@ def create_note_element(step, alter, octave, tie_start=False, tie_stop=False):
     ET.SubElement(pitch, 'octave').text = str(octave)
 
     ET.SubElement(note, 'duration').text = '1'
+    ET.SubElement(note, 'voice').text = str(voice)
     ET.SubElement(note, 'type').text = '64th'
+    ET.SubElement(note, 'stem').text = 'up'
 
-    if tie_start:
-        tie = ET.SubElement(note, 'tie', type="start")
-    if tie_stop:
-        tie = ET.SubElement(note, 'tie', type="stop")
-    
-    notations = None
+    if beam_type:
+        ET.SubElement(note, 'beam', number='1').text = beam_type
+        for i in range(2, 5):
+            ET.SubElement(note, 'beam', number=str(i)).text = beam_type
+
     if tie_start or tie_stop:
+        tie = ET.SubElement(note, 'tie', type='start' if tie_start else 'stop')
         notations = ET.SubElement(note, 'notations')
-    if tie_start:
-        ET.SubElement(notations, 'tied', type="start")
-    if tie_stop:
-        ET.SubElement(notations, 'tied', type="stop")
+        ET.SubElement(notations, 'tied', type='start' if tie_start else 'stop')
+        if tie_start and tie_stop:
+            # Quando a nota ao mesmo tempo fecha um tie e abre outro
+            notations.append(ET.Element('tied', {'type': 'stop'}))
+            notations.append(ET.Element('tied', {'type': 'start'}))
 
     return note
 
-def create_rest_element():
-    rest_note = ET.Element('note')
-    ET.SubElement(rest_note, 'rest')
-    ET.SubElement(rest_note, 'duration').text = '1'
-    ET.SubElement(rest_note, 'type').text = '64th'
-    return rest_note
+
+def create_rest(voice, beam_type=None):
+    rest = ET.Element('note')
+    ET.SubElement(rest, 'rest')
+    ET.SubElement(rest, 'duration').text = '1'
+    ET.SubElement(rest, 'voice').text = str(voice)
+    ET.SubElement(rest, 'type').text = '64th'
+    if beam_type:
+        ET.SubElement(rest, 'beam', number='1').text = beam_type
+        for i in range(2, 5):
+            ET.SubElement(rest, 'beam', number=str(i)).text = beam_type
+    return rest
+
 
 def build_irreducible_xml(music_dict, xml_path):
     score = ET.Element('score-partwise', version='3.1')
@@ -55,6 +73,7 @@ def build_irreducible_xml(music_dict, xml_path):
 
     part = ET.SubElement(score, 'part', id='P1')
 
+    # Organiza todas as notas e pausas
     all_notes = []
     for note_name, data in music_dict.items():
         for instant, duration in zip(data['instants'], data['durations']):
@@ -63,64 +82,88 @@ def build_irreducible_xml(music_dict, xml_path):
 
     measure = None
     measure_number = 1
-    measure_duration = 64  # 4/4, 64 semifusas
+    measure_duration = DIVISIONS * 4  # 4/4
     time_in_measure = 0
     last_instant = 0
+    voice_counter = 1
 
+    events = []
+
+    # Primeiro gera todos os eventos de semifusas (inclusive pausas)
     for instant, group in groupby(all_notes, key=lambda x: x[0]):
         group = list(group)
 
+        # Verifica espaço vazio -> pausa
         if instant > last_instant:
             silence_duration = instant - last_instant
-            for _ in range(silence_duration):
-                if measure is None or time_in_measure >= measure_duration:
-                    measure = ET.SubElement(part, 'measure', number=str(measure_number))
-                    measure_number += 1
-                    attributes = ET.SubElement(measure, 'attributes')
-                    ET.SubElement(attributes, 'divisions').text = str(DIVISIONS)
-                    time = ET.SubElement(attributes, 'time')
-                    ET.SubElement(time, 'beats').text = '4'
-                    ET.SubElement(time, 'beat-type').text = '4'
-                    key = ET.SubElement(attributes, 'key')
-                    ET.SubElement(key, 'fifths').text = '0'
-                    clef = ET.SubElement(attributes, 'clef')
-                    ET.SubElement(clef, 'sign').text = 'G'
-                    ET.SubElement(clef, 'line').text = '2'
-                    time_in_measure = 0
-                measure.append(create_rest_element())
-                time_in_measure += 1
+            for i in range(silence_duration):
+                events.append(('rest', last_instant + i, None))
             last_instant = instant
 
         max_duration = max(duration for _, duration, _ in group)
-
+        # Divide em semifusas
         for i in range(max_duration):
-            if measure is None or time_in_measure >= measure_duration:
-                measure = ET.SubElement(part, 'measure', number=str(measure_number))
-                measure_number += 1
-                attributes = ET.SubElement(measure, 'attributes')
-                ET.SubElement(attributes, 'divisions').text = str(DIVISIONS)
-                time = ET.SubElement(attributes, 'time')
-                ET.SubElement(time, 'beats').text = '4'
-                ET.SubElement(time, 'beat-type').text = '4'
-                key = ET.SubElement(attributes, 'key')
-                ET.SubElement(key, 'fifths').text = '0'
-                clef = ET.SubElement(attributes, 'clef')
-                ET.SubElement(clef, 'sign').text = 'G'
-                ET.SubElement(clef, 'line').text = '2'
-                time_in_measure = 0
+            notes_in_this_instant = [parse_note_name(name) for _, duration, name in group if i < duration]
+            events.append(('note', instant + i, notes_in_this_instant))
+        last_instant = instant + max_duration
 
-            for j, (_, duration, note_name) in enumerate(group):
-                if i < duration:
-                    step, alter, octave = parse_note_name(note_name)
-                    tie_start = (i == 0 and duration > 1)
-                    tie_stop = (i == duration - 1 and duration > 1)
-                    note_element = create_note_element(step, alter, octave, tie_start=tie_start, tie_stop=tie_stop)
-                    if j > 0:
-                        ET.SubElement(note_element, 'chord')
-                    measure.append(note_element)
+    # Agora monta as medidas com beams, ties e tudo mais
+    ongoing_ties = {}
 
-            time_in_measure += 1
-            last_instant += 1
+    for idx, (etype, time, pitches) in enumerate(events):
+        if measure is None or time_in_measure >= measure_duration:
+            measure = ET.SubElement(part, 'measure', number=str(measure_number))
+            measure_number += 1
+            attributes = ET.SubElement(measure, 'attributes')
+            ET.SubElement(attributes, 'divisions').text = str(DIVISIONS)
+            time_tag = ET.SubElement(attributes, 'time')
+            ET.SubElement(time_tag, 'beats').text = '4'
+            ET.SubElement(time_tag, 'beat-type').text = '4'
+            key = ET.SubElement(attributes, 'key')
+            ET.SubElement(key, 'fifths').text = '0'
+            clef = ET.SubElement(attributes, 'clef')
+            ET.SubElement(clef, 'sign').text = 'G'
+            ET.SubElement(clef, 'line').text = '2'
+            time_in_measure = 0
+
+        # Descobre se deve ser begin / continue / end no beam
+        if idx == 0 or events[idx - 1][0] != etype:
+            beam = 'begin'
+        elif idx == len(events) - 1 or events[idx + 1][0] != etype:
+            beam = 'end'
+        else:
+            beam = 'continue'
+
+        if etype == 'rest':
+            measure.append(create_rest(voice=1, beam_type=beam))
+        elif etype == 'note':
+            first = True
+            for step, alter, octave in pitches:
+                tie_start = False
+                tie_stop = False
+
+                key = (step, alter, octave)
+                if key in ongoing_ties:
+                    tie_start = True
+                
+                # Verifica se continua
+                future = any(
+                    key in events[i][2] if events[i][0] == 'note' else False
+                    for i in range(idx+1, len(events))
+                )
+                
+                if future:
+                    tie_stop = True
+
+                if tie_start and not tie_stop:
+                    ongoing_ties.pop(key)
+                elif tie_stop:
+                    ongoing_ties[key] = True
+
+                measure.append(create_single_note(step, alter, octave, voice=1, is_chord=not first, tie_start=tie_start, tie_stop=tie_stop, beam_type=beam))
+                first = False
+
+        time_in_measure += 1
 
     tree = ET.ElementTree(score)
     tree.write(xml_path, encoding='utf-8', xml_declaration=True)
